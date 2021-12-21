b11ec30bb6f2226043fb6539fda9f829c624ae44
Ropon <luopeng@codoon.com>
2021-12-21 10:50:48 +0800

    add color

--- examples/example.go
+++ examples/example.go
@@ -12,31 +12,27 @@ func main() {
 
 	//支持自定义
 	logCfg := &logger.LogCfg{
-		FilePath:  "./log_dir",
+		FilePath:  "./log",
 		FileName:  "ropon.log",
 		Level:     "debug",
 		MaxSize:   100 * 1024 * 1024,
 		SplitFlag: true,
-		TimeDr:    2,
+		TimeDr:    1,
 	}
 	err := logger.InitLog(logCfg)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	for i := 0; i < 1000000; i++ {
+	defer logger.Close()
+	for i := 0; i < 10000000; i++ {
 		logger.Debug("这是一条debug日志，我的名字是：%s，年龄：%d", "Ropon", 18)
 		logger.Info("这是一条Info日志")
 		logger.Warn("这是一条warn日志")
-		logger.Error("这是一条error日志")
-		time.Sleep(time.Second * 15)
+		logger.Error("这是一条error日志 %s", "问题这么严重 。。。。")
+		//logger.Fatal("这是一条fatal日志")
+		//logger.Panic("这是一条panic日志")
+		time.Sleep(time.Second)
 	}
 	select {}
 }
-
-/*
-[2021-08-26 21:18:10.224] [DEBUG] [main.go:main.main] 24 [grn:2] 这是一条debug日志，我的名字是：Ropon，年龄：18
-[2021-08-26 21:18:10.224] [INFO] [main.go:main.main] 25 [grn:2] 这是一条Info日志
-[2021-08-26 21:18:10.224] [WARN] [main.go:main.main] 26 [grn:2] 这是一条warn日志
-[2021-08-26 21:18:10.224] [ERROR] [main.go:main.main] 27 [grn:2] 这是一条warn日志
-*/
